f9ac1ffa41373cfdc152d06a3ffeb440a5a62e88
adam31-7 <adam.ladjal@viacesi.fr>
2025-08-26 19:05:00 +0200

    Added an another card for the project to show

--- components/main/Cardflip.jsx
+++ components/main/Cardflip.jsx
@@ -3,63 +3,68 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 
 const CardFlip = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlippedCard1, setIsFlippedCard1] = useState(false);
+  const [isFlippedCard2, setIsFlippedCard2] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
 
-  function handleFlip() {
-    if (!isAnimating) {
-      setIsFlipped(!isFlipped);
-      setIsAnimating(true);
-    }
-  }
   return (
-    <div  id='projects' className="flex items-center justify-center bg-black h-[800px] cursor-pointer relative" >
+    <div
+      id='projects'
+      className="flex flex-col items-center justify-center bg-black h-[800px] cursor-pointer gap-8 relative"
+    >
+      {/* Carte 1 */}
       <div
-        className="flip-card w-[800px] h-[560px] rounded-md absolute z-[20]"
-        onClick={handleFlip}
+        className="flip-card w-[800px] h-[560px] rounded-md z-[20]"
+        onClick={() => {
+          if (!isAnimating) {
+            setIsFlippedCard1(!isFlippedCard1);
+            setIsAnimating(true);
+          }
+        }}
       >
         <motion.div
           className="flip-card-inner w-[100%] h-[100%]"
           initial={false}
-          animate={{ rotateY: isFlipped ? 180 : 360 }}
-          transition={{ duration: 0.6, animationDirection: "normal" }}
+          animate={{ rotateY: isFlippedCard1 ? 180 : 0 }}
+          transition={{ duration: 0.6 }}
           onAnimationComplete={() => setIsAnimating(false)}
         >
           <div
             className="flip-card-front w-[100%] h-[100%] bg-cover border-[1px] text-white rounded-lg p-4"
-            style={{ backgroundImage: `url(${"/Projet.jpg"})` }}
-          >
-          </div>
-
+            style={{ backgroundImage: `url("/Projet.jpg")` }}
+          />
           <div
             className="flip-card-back w-[100%] h-[100%] bg-cover border-[1px] text-white rounded-lg p-4"
-            style={{ backgroundImage: `url(${"/SpaceCity1.jpg"})` }}
-          >
-          </div>
+            style={{ backgroundImage: `url("/SpaceCity1.jpg")` }}
+          />
         </motion.div>
       </div>
+
+      {/* Carte 2 */}
       <div
-        className="flip-card w-[800px] h-[560px] rounded-md absolute z-[20]"
-        onClick={handleFlip}
+        className="flip-card w-[800px] h-[560px] rounded-md z-[20]"
+        onClick={() => {
+          if (!isAnimating) {
+            setIsFlippedCard2(!isFlippedCard2);
+            setIsAnimating(true);
+          }
+        }}
       >
         <motion.div
           className="flip-card-inner w-[100%] h-[100%]"
           initial={false}
-          animate={{ rotateY: isFlipped ? 180 : 360 }}
-          transition={{ duration: 0.6, animationDirection: "normal" }}
+          animate={{ rotateY: isFlippedCard2 ? 180 : 0 }}
+          transition={{ duration: 0.6 }}
           onAnimationComplete={() => setIsAnimating(false)}
         >
           <div
             className="flip-card-front w-[100%] h-[100%] bg-cover border-[1px] text-white rounded-lg p-4"
-            style={{ backgroundImage: `url(${"/EasySave.jpg"})` }}
-          >
-          </div>
-
+            style={{ backgroundImage: `url("/EasySave.jpg")` }}
+          />
           <div
             className="flip-card-back w-[100%] h-[100%] bg-cover border-[1px] text-white rounded-lg p-4"
-            style={{ backgroundImage: `url(${"/EasySave back.jpg"})` }}
-          >
-          </div>
+            style={{ backgroundImage: `url("/EasySave back.jpg")` }}
+          />
         </motion.div>
       </div>
     </div>
